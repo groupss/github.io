a69ad8fda737809cb90f52ae00f7aa3eb1fe8208
PrinceNoctis <ssaad363@gmail.com>
2018-11-27 13:27:29 -0600

    Update sketch.js

--- menu/sketch.js
+++ menu/sketch.js
@@ -1,66 +1,205 @@
-// 2d arrays grid assigment
-// Talha Prodhan
-// Nov 14, 2018
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
 
-//Extra for Experts: Other than just creating a 2d arrays grid/basic requirement for the assignment,
-
+let tiles;
+let levelBackground;
+let platform, coin,finish,dirt,players;
+let tilesHigh, tilesWide;
+let tileWidth, tileHeight;
+let levelToLoad;
+let lines;
+let stopMove;
+let x, y,px,py;
+let isMovingUp, isMovingDown, isMovingLeft, isMovingRight;
 let gamestate;
 let hoveringButton;
 
+
+function preload() {
+  //load level data
+  levelToLoad = "assets/22.txt";
+  lines = loadStrings(levelToLoad);
+  //load background
+  levelBackground = loadImage("assets/level_background.png");
+  //load tile images
+  platform = loadImage("assets/platform.png");
+  coin = loadImage("assets/coin.png");
+  dirt = loadImage("assets/empty.png");
+  finish = loadImage("assets/finish.png");
+  players = loadImage("assets/player.gif");
+}
+
 function setup() {
-  gamestate = 1;
+  px = 0;
+  py = 0;
+  gamestate = "+";
+  // keep this a 4:3 ratio, or it will stretch in weird ways
+  createCanvas(800, 600);
 
-}
+  tilesHigh = lines.length;
+  tilesWide = lines[0].length;
 
-function draw(){
+  tileWidth = width / tilesWide;
+  tileHeight = height / tilesHigh;
 
-}
-
-function menu() {
-  //This function displayes all of the main menu
-  background(10);
-  // display();
-  //All the menu text stuff
-  fill(0, 255, 0);
-  textSize(55);
-  text("2D PLATFORMER GAME", width/2, 100);
-  textSize(20);
-  text("There are two snakes in a battle.", width/2, 150);
-  text("A green one (WASD controls), and a blue one (IJKL controls)", width/2, 200);
-  text("Eat the red food to get longer", width/2, 250);
-  text("If you hit your tail, or the tail of the other snake, you lose", width/2, 300);
-  text("Press P to pause and have fun", width/2, 350);
-
-
-  //Drawing the start button
-  rectMode(CENTER);
-
-  //If the mouse hovers over the button, using a state variable, it will change color.
-  if (collidePointRect(mouseX, mouseY, width/2 - 150, 450, 300, 100)) {
-    hoveringButton = true;
-    if (mouseIsPressed) {
-      gamestate ++;
-      //If they hit the button, the game will begin
+  tiles = createempty2dArray(tilesWide, tilesHigh);
+  x = width/2;
+  y = height/2;
+  isMovingUp = false;
+  isMovingDown = false;
+  isMovingLeft = false;
+  isMovingRight = false;
+  //put values into 2d array of characters
+  for (let y = 0; y < tilesHigh; y++) {
+    for (let x = 0; x < tilesWide; x++) {
+      let tileType = lines[y][x];
+      tiles[x][y] = tileType;
     }
   }
-  else {
-    hoveringButton = false;
+}
+
+function draw() {
+  movePlayer();
+  if (gamestate === "+") {
+    displayStartScreen();
+  }
+  if (gamestate === 2) {
+    // player();
+    display();
+  }
+}
+
+function display() {
+  if (gamestate === 2){
+    image(levelBackground, 0, 0, width, height);
+
+    for (let y = 0; y < tilesHigh; y++) {
+      for (let x = 0; x < tilesWide; x++) {
+        showTile(tiles[x][y], x, y);
+      }
+    }
+  }
+  // player();
+}
+//
+// function player(){
+//   ellipseMode(CENTER);
+//   ellipse(x,y,tilesWide,tilesHigh);
+//   if (y >= 600 || y <= 0) {
+//     y= y * -1.00001;
+//   }
+//   if (x  >= 800 || x <= 0) {
+//     x= x * -1.00001;
+//   }
+// }
+
+function showTile(location, x, y) {
+  if (location === "#") {
+    image(platform, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "C") {
+    image(coin, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if ( location === "+") {
+    image(dirt, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if ( location === "f") {
+    image(finish, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if ( location === "S") {
+    image(players, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
   }
 
-  if (hoveringButton) {
-    //Changing the fill colors
-    fill(60, 0, 0);
+}
+
+function movePlayer() {
+  if (keyIsDown(68)) { // "a"
+    x += 5;
   }
-  else {
-    fill(255, 0, 0);
+  if (keyIsDown(65)) { // "d"
+    x -= 5;
+  }
+  if (keyIsDown(83)) { // "w"
+    y += 5;
+  }
+  if (keyIsDown(87)) { // "s"
+    y -= 5;
+  }
+}
+
+
+function createempty2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x = 0; x < cols; x++) {
+    randomGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      randomGrid[x].push(0);
+    }
+  }
+  return randomGrid;
+}
+
+function keyPressed() {
+  if (keyCode === 87) { //keycode 87 = w
+    //condition for checking if the top square is white or green/destination
+    if (tiles[py-1][px] === "+" || tiles[py-1][px] === "f") {
+      tiles[py-1][px] = "S";
+      tiles[py][px] = "+";
+      py -= 1; //changes players current y position after moving
+    }
+  }
+  if (keyCode === 65) { //keycode 65 = a
+    //condition for checking if the left square is white or green/destination
+    if (tiles[py][px-1] === "+" || tiles[py][px-1] === "f") {
+      tiles[py][px-1] = "S";
+      tiles[py][px] = "+";
+      px -= 1;  //changes players current x position after moving
+    }
+  }
+  if (keyCode === 83) { //keycode 83 = s
+    //condition for checking if the bottom square is white or green/destination
+    if (tiles[py+1][px] === "+" || tiles[py+1][px] === "f") {
+      tiles[py+1][px] = "S";
+      tiles[py][px] = "+";
+      py += 1;  //changes players current y position after moving
+    }
+  }
+  if (keyCode === 68) { //keycode 68 = d
+    //condition for checking if the right square is white or green/destination
+    if (tiles[py][px+1] === "+" || tiles[py][px+1] === "f") {
+      tiles[py][px+1] = "S";
+      tiles[py][px] = "+";
+      px += 1;  //changes players current x position after moving
+    }
+  }
+}
+
+
+function displayStartScreen() {
+  let buttonWidth = 400;
+  let buttonHeight = 200;
+  let leftSide = width / 2 - buttonWidth / 2;
+  let topSide = height / 2 - buttonHeight / 2;
+  let rightSide = leftSide + buttonWidth;
+  let bottomSide = topSide + buttonHeight;
+
+  fill(0);
+  if (mouseX >= leftSide && mouseX <= rightSide && mouseY >= topSide && mouseY <= bottomSide) {
+    fill(125);
+    if (mouseIsPressed) {
+      gamestate = 2;
+    }
   }
 
-  rect(width/2, 500, 300, 100);
-  //Start button
-  rectMode(CORNER);
-
-  fill(255);
-  textSize(35);
-  text("START", width/2, 510);
-  //Putting text into the button
+  rect(leftSide, topSide, buttonWidth, buttonHeight);
 }
